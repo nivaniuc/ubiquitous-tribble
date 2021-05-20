9de33c96fc3d0b9fb1182551ee495698cd47f5f4
nivaniuc <nivaniucc@gmail.com>
2021-05-20 13:39:41 -0400

    Working on rendering the employee manager html

--- dev/app.js
+++ dev/app.js
@@ -4,8 +4,57 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+const outputDir = path.resolve(__dirname, "output")
+const writeFileAsync = util.promisify(fs.writeFile)
+
+
+const questions = [
+
+    {
+        type: "input",
+        message: "What is Manger Name:",
+        name: "name",
+    },
+
+    {
+        type: "input",
+        message: "Manager Id",
+        name: "id"
+    },
+
+    {
+        type: "input",
+        message: "Manager EmailAddress",
+        name: "email"
+    },
+
+
+    {
+        type: "input",
+        message: "Manager office number",
+        name: "officeNumber"
+    },
+
+];
+
+class App {
+
+    askQuestions() {
+        return inquirer.prompt(questions).then( val => {
+            let manager = new Manager(val.name, val.id, val.email, val.officeNumber)
+            console.log(`line 50 app.js ${val.name}, ${val.id}, ${val.email}, ${val.officeNumber}`)
+            console.log(`\n output path ${manager} \n`)
+            render(manager);
+        })
+    }
+}
+
+let test = new App()
+test.askQuestions()
--- dev/lib/htmlRenderer.js
+++ dev/lib/htmlRenderer.js
@@ -1,35 +1,77 @@
 const path = require("path");
 const fs = require("fs");
+const util = require("util")
 
 const templatesDir = path.resolve(__dirname, "../templates");
 
 const render = employees => {
+  console.log(`Inside render employees ${employees}`)
   const html = [];
+  
+  html.push(employees)
+  console.log(`Inside render employees  html push ${html}`)
+  html.filter(employee => {
+    if (employee.getRole() === "Manager") {
+      console.log("Yes I am the manager")
+      html.map(async manager =>{
+        console.log(`line 21 html.map manager ${manager}`)
 
-  html.push(employees
-    .filter(employee => employee.getRole() === "Manager")
-    .map(manager => renderManager(manager))
-  );
-  html.push(employees
-    .filter(employee => employee.getRole() === "Engineer")
-    .map(engineer => renderEngineer(engineer))
-  );
-  html.push(employees
-    .filter(employee => employee.getRole() === "Intern")
-    .map(intern => renderIntern(intern))
-  );
+    if (!fs.existsSync(outputDir)) {
+      console.log(`Making Directory`)
+      fs.mkdirSync(outputDir);
+      await writeFileAsync("./output/team.html", renderMain(renderManager(manager)))
+    } else {
+      await writeFileAsync("./output/team.html", renderMain(renderManager(manager)))
+
+    }
+    } )
+
+    } else {
+      console.log("I am not a manager")
+    }
+  })
+
+  console.log(`After generating html file before render main`)
+  console.log(`After generating html file before render main ${renderMain(html.join(""))}`)
+
+  // html.push(employees
+  //   .filter(employee => employee.getRole() === "Manager")
+  //   .map(manager => renderManager(manager))
+  // );
+  // html.push(employees
+  //   .filter(employee => employee.getRole() === "Engineer")
+  //   .map(engineer => renderEngineer(engineer))
+  // );
+  // html.push(employees
+  //   .filter(employee => employee.getRole() === "Intern")
+  //   .map(intern => renderIntern(intern))
+  // );
 
   return renderMain(html.join(""));
 
 };
 
-const renderManager = manager => {
+const renderManager =async manager => {
+  console.log(`I am renderManager ${manager}`)
   let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
   template = replacePlaceholders(template, "name", manager.getName());
   template = replacePlaceholders(template, "role", manager.getRole());
   template = replacePlaceholders(template, "email", manager.getEmail());
   template = replacePlaceholders(template, "id", manager.getId());
   template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
+  console.log(`I am renderManager template : ${template}`)
+  
+  let teamHTML = template
+
+  if (!fs.existsSync(outputDir)) {
+    console.log(`Making Directory`)
+    fs.mkdirSync(outputDir);
+    await writeFileAsync("./output/team.html", teamHTML)
+  }else
+  {
+    await writeFileAsync("./output/team.html", teamHTML)
+
+  }
   return template;
 };
 
